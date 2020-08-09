08e3c8a55224de8adb6a7eab9645d023c7a8c17c
shubham9019 <shubham9019@gmail.com>
2020-08-09 18:16:12 +0530

    Basic Design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import post from'./post.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />  
+      </div>
+      <post/>
     </div>
   );
 }
---
+++ src/post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function post() {
+    return (
+        <div>
+            <h3>Username</h3>
+            <img src="https://upload.wikimedia.org/wikipedia/en/5/5f/Original_Doge_meme.jpg" alt=""/>
+            
+        </div>
+    )
+}
+
+export default post
